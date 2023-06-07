19ec144e84c7ea18884b2a004a0bc3998394e7fd
KatOli4ka <KatOli4ka87@mail.ru>
2023-06-07 15:18:21 +0500

    Initial commit

---
+++ src/main/java/com/gridnine/testing/flight_module/FlightModuleApplication.java
@@ -0,0 +1,13 @@
+package com.gridnine.testing.flight_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gridnine/testing/flight_module/FlightModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridnine.testing.flight_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
